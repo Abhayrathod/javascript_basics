f06ab353427b56e0d5a644b167934521820bd48d
DESKTOP-E9EMJEK\sl pc <abhayr.brainerhub@gmail.com>
2022-08-10 18:38:26 +0530

    updated code

---
+++ 6_for_loops.js
@@ -0,0 +1,40 @@
+// // Program to add first n natural numbers
+// let sum = 0
+// let n = prompt("Enter the value of n")
+// n = Number.parseInt(n)
+// for (let i = 0; i < n; i++) {
+//   sum += (i + 1)
+//   // console.log((i+1), "+")
+// }
+// console.log("Sum of first " + n + " natural numbers is " + sum)
+// // console.log(i)
+
+
+// #--------------for loop in browser
+
+// let num = prompt("enter the numbers")
+// n = Number.parseInt(num)
+// for (i = 0; i<500; i++){
+//     console.log(i)
+// }
+
+
+
+
+// let obj = {
+//   harry: 90,
+//   shubh: 45,
+//   shivika: 56,
+//   ritika: 57,
+//   shiv: 23
+// }
+
+// // For in loop
+// for (let a in obj) {
+//   console.log("Marks of " + a + " are " + obj[a])
+// }
+
+// For of loop
+// for (let b of "Harry") {
+//   console.log(b)
+// }
---
+++ 7_while_loops.js
@@ -0,0 +1,22 @@
+// while loop
+/*
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+*/
+// while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+
+// Do while me pehle ek bar code chalega fir conditinon check hogi
+let i = 10;
+do{
+  console.log(i)
+  i++;
+}while (i < n) 
